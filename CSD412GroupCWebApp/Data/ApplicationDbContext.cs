c2317531e557c642958ff83b1f29a30e0091cbb4
Noah Greer <noah.greer@gmail.com>
2019-11-10 22:22:43 -0800

    Scaffold ApplicationUser pages

--- CSD412GroupCWebApp/Data/ApplicationDbContext.cs
+++ CSD412GroupCWebApp/Data/ApplicationDbContext.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using CSD412GroupCWebApp.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CSD412GroupCWebApp.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
         }
     }
 }
