fae199937e9d5de9b3e880657d35851c70abab14
Noah Greer <noah.greer@gmail.com>
2019-11-10 22:44:34 -0800

    Migrated custom user data

--- CSD412GroupCWebApp/Data/Migrations/20191111063958_CreateCustomUserData.Designer.cs
+++ CSD412GroupCWebApp/Data/Migrations/20191111063958_CreateCustomUserData.Designer.cs
@@ -4,14 +4,16 @@ using CSD412GroupCWebApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CSD412GroupCWebApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191111063958_CreateCustomUserData")]
+    partial class CreateCustomUserData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
