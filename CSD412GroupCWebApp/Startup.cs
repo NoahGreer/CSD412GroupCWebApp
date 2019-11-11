ae34293bd09d39df633903c9d14a861bf6dc17f0
Noah Greer <noah.greer@gmail.com>
2019-11-10 16:19:25 -0800

    Added ApplicationUser

--- CSD412GroupCWebApp/Startup.cs
+++ CSD412GroupCWebApp/Startup.cs
@@ -12,6 +12,7 @@ using CSD412GroupCWebApp.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using CSD412GroupCWebApp.Models;
 
 namespace CSD412GroupCWebApp
 {
@@ -30,7 +31,7 @@ namespace CSD412GroupCWebApp
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
