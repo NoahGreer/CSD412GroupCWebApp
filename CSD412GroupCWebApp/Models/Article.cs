c639fd63ec12e3e2b76e854869582b272344d42d
SarehAkbarpour <56125704+SarehAkbarpour@users.noreply.github.com>
2019-11-11 16:09:07 -0800

    Create Article.cs

---
+++ CSD412GroupCWebApp/Models/Article.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSD412GroupCWebApp.Models
+{
+    public class Article
+    {
+        public long Id { get; set; }
+        public Blog Blog { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        [Display(Name = "URL Slug")]
+        public string UrlSlug { get; set; }
+        public Category Categories { get; set; }
+        [Display(Name = "Date Posted")]
+        public string DatePosted { get; set; }
+    }
+}
