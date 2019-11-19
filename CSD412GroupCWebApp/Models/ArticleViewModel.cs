65745c45b7febeaf233b794b08580708100a8926
Noah Greer <noah.greer@gmail.com>
2019-11-18 21:38:45 -0800

    Added ArticleViewModel

---
+++ CSD412GroupCWebApp/Models/ArticleViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSD412GroupCWebApp.Models
+{
+    public class ArticleViewModel
+    {
+        public Article Article { get; set; }
+        public List<Category> Categories { get; set; }
+
+        // This method is used to populate the options of the DropdownList
+        public IEnumerable<SelectListItem> GetCategoriesAsSelectListItems()
+        {
+            // Display the "Name" of the Category in DropDown 
+            // Use "Id" for the selected value
+            return new SelectList(Categories, "Id", "Name");
+        }
+    }
+}
