3c34b2fa26a4f2a6098445035043231bcdf301c4
Noah Greer <noah.greer@gmail.com>
2019-11-24 13:02:21 -0800

    Added RoleViewModel for editing roles

---
+++ CSD412GroupCWebApp/Models/RoleViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSD412GroupCWebApp.Models
+{
+    public class RoleViewModel
+    {
+        public IdentityRole Role { get; set; }
+        public List<ApplicationUser> Members { get; set; }
+        public string[] AddedUserIds { get; set; } = new string[0];
+        public List<ApplicationUser> NonMembers { get; set; }
+        public string[] RemovedUserIds { get; set; } = new string[0];
+    }
+}
