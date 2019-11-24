2832cc1343d8017eeea99c77fc948ce7e051c5cc
Noah Greer <noah.greer@gmail.com>
2019-11-24 13:04:32 -0800

    Added RolesController and views

---
+++ CSD412GroupCWebApp/Controllers/RolesController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CSD412GroupCWebApp.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace CSD412GroupCWebApp
+{
+    public class RolesController : Controller
+    {
+        private RoleManager<IdentityRole> _roleManager;
+        private UserManager<ApplicationUser> _userManager;
+
+        public RolesController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        // GET: Roles
+        public async Task<IActionResult> Index()
+        {
+            return View(await _roleManager.Roles.ToListAsync());
+        }
+
+        // GET: Roles/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            var users = _userManager.Users;
+            var members = new List<ApplicationUser>();
+            var nonMembers = new List<ApplicationUser>();
+            foreach (var user in users)
+            {
+                bool isInRole = await _userManager.IsInRoleAsync(user, role.Name);
+                if (isInRole)
+                {
+                    members.Add(user);
+                }
+                else
+                {
+                    nonMembers.Add(user);
+                }
+            }
+
+            RoleViewModel roleViewModel = new RoleViewModel()
+            {
+                Role = role,
+                Members = members,
+                NonMembers = nonMembers
+            };
+
+            return View(roleViewModel);
+        }
+
+        // POST: Roles/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(RoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result;
+                foreach (string addedUserId in model.AddedUserIds)
+                {
+                    var user = await _userManager.FindByIdAsync(addedUserId);
+                    if (user != null)
+                    {
+                        result = await _userManager.AddToRoleAsync(user, model.Role.Name);
+                        if (!result.Succeeded)
+                        {
+                            Errors(result);
+                        }
+                    }
+                }
+
+                foreach (string removedUserId in model.RemovedUserIds)
+                {
+                    var user = await _userManager.FindByIdAsync(removedUserId);
+                    if (user != null)
+                    {
+                        result = await _userManager.RemoveFromRoleAsync(user, model.Role.Name);
+                        if (!result.Succeeded)
+                        {
+                            Errors(result);
+                        }
+                    }
+                }
+            }
+
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return await Edit(model.Role.Id);
+            }
+        }
+
+        private void Errors(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+        }
+    }
+}
