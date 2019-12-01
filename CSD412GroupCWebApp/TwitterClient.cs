10dd7b941296e3b48ed25f474305594685117858
Nathan O'Brien <mugatu9@gmail.com>
2019-11-30 16:42:51 -0800

    Added Twitter Client

---
+++ CSD412GroupCWebApp/TwitterClient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LinqToTwitter;
+
+namespace CSD412GroupCWebApp
+{
+    public class TwitterClient
+    {
+        private TwitterContext context;
+
+        private static string url;
+
+        public TwitterClient(SingleUserInMemoryCredentialStore credentials)
+        {
+            var auth = new SingleUserAuthorizer
+            {
+                CredentialStore = credentials
+            };
+
+            context = new TwitterContext(auth);
+        }
+
+        public void SetLink(string newUrl)
+        {
+            url = newUrl;
+        }
+
+        public async Task<Status> SendTweet()
+        {
+            return await context.TweetAsync(url);
+        }
+    }
+}
