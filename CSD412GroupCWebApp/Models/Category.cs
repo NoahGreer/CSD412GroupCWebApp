feaad7d14bfe46d3e630c4f5c5a161a2d7f0aa2e
Noah Greer <noah.greer@gmail.com>
2019-12-02 20:02:29 -0800

    Use ArticleCategory in Category

--- CSD412GroupCWebApp/Models/Category.cs
+++ CSD412GroupCWebApp/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,7 @@ namespace CSD412GroupCWebApp.Models
         public long Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public virtual ICollection<Article> Articles { get; set; }
+        [Display(Name = "Articles")]
+        public virtual ICollection<ArticleCategory> ArticleCategories { get; set; }
     }
 }
