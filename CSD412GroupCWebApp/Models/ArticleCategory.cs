3cfacfc8826c58d0ff0166aa707dd783489c121a
Noah Greer <noah.greer@gmail.com>
2019-12-02 19:43:44 -0800

    Create joining entity ArticleCategory between Articles and Categories

---
+++ CSD412GroupCWebApp/Models/ArticleCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSD412GroupCWebApp.Models
+{
+    public class ArticleCategory
+    {
+        public long ArticleId { get; set; }
+        public Article Article { get; set; }
+        public long CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
