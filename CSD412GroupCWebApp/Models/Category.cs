a18ec25a50b4072c5fcf6cda5f08175f41e6cd46
Noah Greer <noah.greer@gmail.com>
2019-12-02 18:38:12 -0800

    Added Articles navigation property to Category to create many to many relationship

--- CSD412GroupCWebApp/Models/Category.cs
+++ CSD412GroupCWebApp/Models/Category.cs
@@ -10,5 +10,6 @@ namespace CSD412GroupCWebApp.Models
         public long Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public virtual ICollection<Article> Articles { get; set; }
     }
 }
